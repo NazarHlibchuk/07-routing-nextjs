082218b9c195ccdd7db85d678c7266a1a741a413
Nazar <123.nazar79@gmail.com>
2025-09-27 13:04:55 +0200

    add

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,13 +4,13 @@
 
 import { useState } from "react";
 import NoteList from "@/components/NoteList/NoteList";
-import { getNotes, Note } from "@/lib/api";
+import { fetchNotes, Note } from "@/lib/api";
 
 const Notes = () => {
   const [notes, setNotes] = useState<Note[]>([]);
 
   const handleClick = async () => {
-    const response = await getNotes();
+    const response = await fetchNotes();
     if (response?.notes) {
       setNotes(response.notes);
     }
