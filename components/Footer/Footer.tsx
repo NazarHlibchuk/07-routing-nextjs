da80eee7f31ccac16e15a4e1d12701aceda29544
Nazar <123.nazar79@gmail.com>
2025-09-29 19:00:53 +0200

    add

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,17 +5,19 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className={css.footer}>
-      <div className={css.content}>
-        <p>© {currentYear} NoteHub. All rights reserved.</p>
-        <div className={css.wrap}>
-          <p>Developer: your name</p>
-          <p>
-            Contact us: <a href="mailto:student@notehub.app">student@notehub.app</a>
-          </p>
-        </div>
-      </div>
-    </footer>
+<footer className={css.footer}>
+  <div className={css.content}>
+    <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
+    <div className={css.wrap}>
+      <p>Developer: Nazar Hlibchuk</p>
+      <p>
+        Contact us:
+        <a href="mailto:student@notehub.app">student@notehub.app</a>
+      </p>
+    </div>
+  </div>
+</footer>
+
   );
 };
 
