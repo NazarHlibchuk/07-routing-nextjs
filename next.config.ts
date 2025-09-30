9d749ad92481f90606f685803c493b0a1026e70a
Nazar <123.nazar79@gmail.com>
2025-09-30 17:19:49 +0200

    add

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-import { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',                 // для статичного експорту
-  basePath: '/06-notehub-nextjs',   // назва твого репозиторію
-  assetPrefix: '/06-notehub-nextjs/', // префікс для стилів і JS
+  /* config options here */
 };
 
 export default nextConfig;
