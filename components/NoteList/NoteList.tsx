419699c56a88f558611a430911fc42eb881a346b
Nazar <123.nazar79@gmail.com>
2025-09-30 17:44:37 +0200

    add

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -24,17 +24,19 @@ const NoteList = ({ notes, onDeleteSuccess }: NoteListProps) => {
     <ul className={css.noteList}>
       {notes.map((note) => (
         <li key={note.id} className={css.noteItem}>
-          <Link href={`/notes/${note.id}`} className={css.noteLink}>
-            <h3 className={css.title}>{note.title}</h3>
-            <p className={css.content}>{note.content}</p>
-            <p className={css.tag}>Tag: {note.tag}</p>
-          </Link>
-          <button
-            className={css.deleteButton}
-            onClick={() => mutation.mutate(note.id)}
-          >
-            Delete
-          </button>
+          <div className={css.footer}>
+            <Link href={`/notes/${note.id}`} className={css.noteLink}>
+              <h3 className={css.title}>{note.title}</h3>
+              <p className={css.content}>{note.content}</p>
+              <span className={css.tag}>{note.tag}</span>
+            </Link>
+            <button
+              className={css.deleteButton}
+              onClick={() => mutation.mutate(note.id)}
+            >
+              Delete
+            </button>
+          </div>
         </li>
       ))}
     </ul>
