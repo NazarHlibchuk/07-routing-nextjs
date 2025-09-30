a6569e7e5de2da21a5355897fbbf5672fb20cf05
Nazar <123.nazar79@gmail.com>
2025-09-30 18:45:39 +0200

    add finish file

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className={css.wrap}>
           <p>Developer: Nazar Hlibchuk</p>
           <p>
-            Contact us: <a href="mailto:captkit@ukr.net">captkit@ukr.net</a>
+            Contact us: <a href="mailto:123.nazar79@gmail.com">123.nazar79@gmail.com</a>
           </p>
         </div>
       </div>
