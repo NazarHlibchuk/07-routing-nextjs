80cbfc4f4845213c3b1f5e0b33d112e172931169
Nazar <123.nazar79@gmail.com>
2025-10-07 12:30:59 +0200

    Add

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -13,7 +13,7 @@ export default function SidebarNotes() {
       <ul className={css.menuList}>
         {/* All notes */}
         <li className={css.menuItem}>
-          <Link href="/notes/filter" className={css.menuLink}>
+          <Link href="/notes/filter/All" className={css.menuLink}>
             {ALL_TAG}
           </Link>
         </li>
